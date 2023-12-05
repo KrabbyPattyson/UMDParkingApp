b827f54ed83a806cac04507620339584b0db5626
John A McAdoo <john.a.mcadoo@gmail.com>
2023-12-04 20:47:22 -0500

    -John added buttons to activity_main.xml and some functions in main activity which will eventually switch the screens(views)

--- app/src/main/java/com/example/umdparkingapp/MainActivity.kt
+++ app/src/main/java/com/example/umdparkingapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.umdparkingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.Button
 
+private lateinit var buttonMap: Button
+private lateinit var buttonSettings: Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onMapClick(v:View){
+
+    }
+    fun onSettingsClick(v:View){
+
     }
 }
