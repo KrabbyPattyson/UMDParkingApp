4d6154ee4a935cb6d0e10ef478ba836f066cc47a
John A McAdoo <john.a.mcadoo@gmail.com>
2023-12-04 21:04:51 -0500

    -John added buttons to activity_main.xml and went to the view (either Map or settings) after the user clicks on the button

--- app/src/main/java/com/example/umdparkingapp/MainActivity.kt
+++ app/src/main/java/com/example/umdparkingapp/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.umdparkingapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.util.Log
 import android.view.View
 import android.widget.Button
+
 
 private lateinit var buttonMap: Button
 private lateinit var buttonSettings: Button
@@ -12,11 +14,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        var mapButtonClick = findViewById<Button>(R.id.buttonMap)
+        var settingsButtonClick = findViewById<Button>(R.id.buttonSettings)
+
+        mapButtonClick.setOnClickListener {
+            val intent = Intent(this, MapActivity::class.java)
+            startActivity(intent)
+            Log.w("MainActivity", "Sent to MapActivity view?")
+        }
+
+
+        settingsButtonClick.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+            Log.w("MainActivity", "Sent to SettingsActivity view?")
+        }
+    }/*
     fun onMapClick(v:View){
 
     }
     fun onSettingsClick(v:View){
 
-    }
+    }*/
 }
