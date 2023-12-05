5831533b8e418f2380a56f36adc7e34f2dfa9905
KrabbyPattyson <originalbrycekrispies@gmail.com>
2023-12-04 21:39:32 -0500

    Basic functionality for Settings.

--- app/src/main/java/com/example/umdparkingapp/SettingsActivity.kt
+++ app/src/main/java/com/example/umdparkingapp/SettingsActivity.kt
@@ -1,11 +1,16 @@
 package com.example.umdparkingapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+    }
+
+    fun endActivity(v: View){
+        finish()
     }
 }
