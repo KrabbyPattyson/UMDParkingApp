3b2ffe2a211c77ec8d408817e26b3a510c77c758
John A McAdoo <john.a.mcadoo@gmail.com>
2023-12-06 15:03:42 -0500

    -John added calendar view on main idk where its supposed to be

--- app/src/main/java/com/example/umdparkingapp/SettingsActivity.kt
+++ app/src/main/java/com/example/umdparkingapp/SettingsActivity.kt
@@ -3,6 +3,10 @@ package com.example.umdparkingapp
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
+
 // Project 4 game.kt is the one with persistent data aka SharedPreferences that we will use here
 
 class SettingsActivity : AppCompatActivity() {
