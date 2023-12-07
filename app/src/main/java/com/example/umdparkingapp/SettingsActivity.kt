1d5ce42acfb91060dd8d4fbe38bfac94efa49cbc
John A McAdoo <john.a.mcadoo@gmail.com>
2023-12-07 16:34:23 -0500

    -John added an ad in settings view... It shows up on the design and if you change the background to a color it will show the banner, but not an add... perhaps we may have to get a real add Application ID or smth idk...

--- app/src/main/java/com/example/umdparkingapp/SettingsActivity.kt
+++ app/src/main/java/com/example/umdparkingapp/SettingsActivity.kt
@@ -6,15 +6,43 @@ import androidx.appcompat.app.AppCompatActivity
 import android.content.Context
 import android.content.SharedPreferences
 import android.util.Log
+import android.widget.LinearLayout
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
 
 // Project 4 game.kt is the one with persistent data aka SharedPreferences that we will use here
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var adView : AdView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        //createAd( )
     }
 
+    fun createAd( ) {
+        Log.w("SettingsActivity", "in createAd")
+        adView = AdView( this )
+        var adSize : AdSize = AdSize( AdSize.FULL_WIDTH, AdSize.AUTO_HEIGHT )
+        adView.setAdSize( adSize )
+
+        var adUnitId : String = // "ca-app-pub-3940256099942544/1033173712"
+            "ca-app-pub-3940256099942544/3347511713"
+        adView.adUnitId = adUnitId
+
+        var builder : AdRequest.Builder = AdRequest.Builder( )
+        builder.addKeyword( "parking" ).addKeyword( "car" )
+        var request : AdRequest = builder.build()
+
+        /*// add adView to linear layout
+        var layout : LinearLayout = findViewById( R.id.adView )
+        layout.addView( adView )
+*/
+        // load the ad
+        adView.loadAd( request )
+    }
     fun endActivity(v: View){
         finish()
     }
