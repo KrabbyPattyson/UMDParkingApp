097a24932ab8f51b84e31904fa96fa9eb35f981c
John A McAdoo <john.a.mcadoo@gmail.com>
2023-12-11 09:08:58 -0500

    -John doesnt compile cuz idk how to extract the LotKey from SharedPref... Just need to replace those commments with what we actually do.

--- app/src/main/java/com/example/umdparkingapp/MapActivity.kt
+++ app/src/main/java/com/example/umdparkingapp/MapActivity.kt
@@ -22,17 +22,37 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
+        settings = Settings(this)
+        settings.sayPreferences()
+
         var fragment : SupportMapFragment =
             supportFragmentManager.findFragmentById( R.id.map ) as SupportMapFragment
         fragment.getMapAsync( this )
+
+
     }
 
     override fun onMapReady(p0: GoogleMap) {
         Log.w( "MainActivity", "Inside onMapReady" )
         map = p0
         var umd : LatLng = LatLng( 38.9860, -76.9451 )
+        var lot1: LatLng = LatLng(38.988127,-76.949295)
+        var lot2: LatLng = LatLng(38.994227,-76.948361)
+        var lot3: LatLng = LatLng(38.988189,-76.953328)
+        var lot4: LatLng = LatLng(38.985521,-76.954336)
+        var lot5: LatLng = LatLng(38.985095,-76.954872)
 
-
+        if(/*SharedPreferences.LotKey == 1*/){
+            map.addMarker(MarkerOptions().position(lot1).title("Lot 1").snippet("Your parking permit is here"))
+        } else if(/*SharedPreferences.LotKey == 2*/){
+            map.addMarker(MarkerOptions().position(lot2).title("Lot 2").snippet("Your parking permit is here"))
+        }else if(/*SharedPreferences.LotKey == 3*/){
+            map.addMarker(MarkerOptions().position(lot3).title("Lot 3").snippet("Your parking permit is here"))
+        }else if(/*SharedPreferences.LotKey == 4*/){
+            map.addMarker(MarkerOptions().position(lot4).title("Lot 4").snippet("Your parking permit is here"))
+        }else if(/*SharedPreferences.LotKey == 5*/){
+            map.addMarker(MarkerOptions().position(lot5).title("Lot 5").snippet("Your parking permit is here"))
+        }
         // add a circle
 
         var options : CircleOptions = CircleOptions( )
@@ -41,6 +61,8 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         options.strokeWidth( 10.0f )
         options.strokeColor( Color.RED )
         map.addCircle( options )
+
+        if(settings.)
 
         // add a marker
         // var marker : MarkerOptions = MarkerOptions( )
@@ -80,5 +102,9 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             super.onError(errorMessage)
             Log.w( "MainActivity", "error: " + errorMessage )
         }
+    }
+
+    companion object {
+        lateinit var settings : Settings
     }
 }
